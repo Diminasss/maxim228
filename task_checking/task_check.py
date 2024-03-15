f71b19b4e6e62f1d68cdf753b0f230930ba04717
Lezhev <lezhevmatvey@ya.ru>
2024-03-16 00:19:28 +0300

    multiple cheking added

--- task_checking/task_check.py
+++ task_checking/task_check.py
@@ -4,7 +4,6 @@ import subprocess
 def check_file(file_path, expected_output):
     result = subprocess.run(['python', file_path], capture_output=True, text=True)
     output = result.stdout
-
-    print(result)
+    print(output, expected_output)
     return output == expected_output
 
