05ede73fb145b67935e78323472e3b5cd72bbc6a
Lezhev <lezhevmatvey@ya.ru>
2024-03-15 18:58:09 +0300

    paths fixed

--- task_checking/clone_repo.py
+++ task_checking/clone_repo.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def clone(folder_name, repo_url):
-    destination_path = f"D:/Neo.edu/cloned_repos/{folder_name}"
+    destination_path = f"cloned_repos/{folder_name}"
 
     command = ["git", "clone", repo_url, destination_path]
 
--- task_checking/run_command_scr.py
+++ task_checking/run_command_scr.py
@@ -6,17 +6,17 @@ from task_check import check_file
 
 folder_name = datetime.datetime.now().strftime("%H%M%S")
 file_path = "https://github.com/lezhev/Neo.edu"
-extended_output = "15"
+expected_output = "15"
 task_input = "sum_process([1, 2, 3, 4, 5])"
 
 clone(folder_name, file_path)
 
-with open(f"D:/Neo.edu/cloned_repos/{folder_name}/main.py", "a") as file:
+with open(f"cloned_repos/{folder_name}/main.py", "a") as file:
     file.write(f"print({task_input})")
     file.flush()
     file.close()
 
-if check_file(f"D:/Neo.edu/cloned_repos/{folder_name}/main.py", extended_output):
+if check_file(f"cloned_repos/{folder_name}/main.py", expected_output):
     print("ok")
 else:
     print("ne ok")
