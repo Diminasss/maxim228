c967778a90534fc9f50ed9901caa49f5bb233d4a
Diminas <pilluping@gmail.com>
2024-03-16 06:11:43 +0300

    Полноценная работа чектест

--- main.py
+++ main.py
@@ -121,29 +121,34 @@ def create_course() -> tuple[wrappers.Response, int]:
 test_initialisation("Python", "Сделайте пацанский тест", ["merge_sort([4, 2, 7, 1, 9, 5, 3, 8, 6])\n", "merge_sort([4, 2, 7, 1, 5, 3, 6])\n", "merge_sort([4, 2, 1, 5, 3, 6])\n"], ["[1, 2, 3, 4, 5, 6, 7, 8, 9]\n", "[1, 2, 3, 4, 5, 6, 7]\n", "[1, 3, 4, 5, 6]\n"])
 
 
-# # @app.route('/checkhomework', methods=['POST'])
-# def check_test(test_id, user_id, repo_url):# -> tuple[wrappers.Response, int]:
-#     # test_input_info: dict = request.get_json()
-#     #
-#     # test_id: int = test_input_info.get('test_id')
-#     # user_id: int = test_input_info.get('user_id')
-#     # repo_url: str = test_input_info.get('repo_url')
-#
-#     task_input: str = get_from_postgresql_test_table("tests", test_id, "input_data")
-#     expected_output: str = get_from_postgresql_test_table("tests", test_id, "output_data")
-#     result = check(file_path=repo_url, task_input=task_input, expected_output=expected_output)
-#     result = str(result.count(True)) + "/" + str(len(result))
-#
-#     last_result = get_from_postgresql_test_table("tests", test_id, "users_with_marks")
-#     if last_result is not None:
-#         last_result = json.loads(last_result)
-#     else:
-#         edit_test_sqlite_table("tests", test_id, "users_with_marks", json.dump({user_id: result}))
-#     print(last_result)
-#
-#     #return jsonify({"response": result}), 200
-#
+# @app.route('/checkhomework', methods=['POST'])
+def check_test() -> tuple[wrappers.Response, int]:
+    test_input_info: dict = request.get_json()
+
+    test_id: int = test_input_info.get('test_id')
+    user_id: int = test_input_info.get('user_id')
+    repo_url: str = test_input_info.get('repo_url')
+
+    task_input: str = get_from_postgresql_test_table("tests", test_id, "input_data")
+    expected_output: str = get_from_postgresql_test_table("tests", test_id, "output_data")
+    result = check(file_path=repo_url, task_input=task_input, expected_output=expected_output)
+    result = str(result.count(True)) + "/" + str(len(result))
+    last_result = get_from_postgresql_test_table("tests", test_id, "users_with_marks")
+    if last_result is not None:
+        if user_id not in last_result:
+            last_result[user_id] = result
+            edit_test_sqlite_table("tests", test_id, "users_with_marks", json.dumps(last_result))
+        else:
+            pass
+    else:
+        new_result = {user_id: result}
+        edit_test_sqlite_table("tests", test_id, "users_with_marks", json.dumps(new_result))
+
+    return jsonify({"response": result}), 200
+
+
 # check_test(1, "2", "https://github.com/lezhev/Neo.edu")
+
 
 @app.route('/test', methods=['POST'])
 def get_test() -> tuple[wrappers.Response, int]:
@@ -157,7 +162,7 @@ def get_test() -> tuple[wrappers.Response, int]:
     # users_with_marks: str = get_from_postgresql_test_table("tests", test_id, "test_name")
     return jsonify({"test_id": test_id, "user_id": user_id, "test_name": test_name, "test_text": test_text, "users_with_marks": "2/3"}), 200
 
-get_test(1, 2)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
